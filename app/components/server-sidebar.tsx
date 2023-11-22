19675a1d7d992271f8bd2f6bfc0da42829679a24
Boris <47457848+boris-chb@users.noreply.github.com>
2023-11-22 12:45:16 +0000

    feat: implement navlink component

--- app/components/server-sidebar.tsx
+++ app/components/server-sidebar.tsx
@@ -1,36 +1,63 @@
 "use client";
 import DiscordIcon from "@/app/components/discord-icon";
+import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+
+const servers = [
+  { id: 1, img: "next.svg" },
+  { id: 2, img: "vercel.svg" },
+];
 
 export default function ServersSidebar() {
   const pathname = usePathname();
 
   return (
     <nav className="bg-gray-900 p-3 overflow-y-scroll flex flex-col gap-2">
-      <Link href={"/"}>
-        <div
-          className={`${
-            pathname === "/"
-              ? "rounded-2xl bg-brand text-white"
-              : "text-gray-100 hover:rounded-2xl hover:bg-brand hover:text-white"
-          } bg-gray-700  w-12 h-12 rounded-3xl  flex items-center justify-center  transition-all duration-200`}
-        >
-          <DiscordIcon className="w-7 h-5" />
-        </div>
-      </Link>
+      <NavLink href="/">
+        <DiscordIcon className="h-5 w-7" />
+      </NavLink>
 
-      <Link href="/servers/1">
+      <hr className="border-t-white/[.06] border-t-2 rounded mx-2" />
+
+      {servers.map(({ id, img }) => (
+        <NavLink key={id} href={`/servers/${id}`}>
+          <Image src={`/${img}`} fill alt="server icon" />
+        </NavLink>
+      ))}
+    </nav>
+  );
+}
+
+function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+
+  return (
+    <Link href={href} className="relative group rounded-2xl">
+      <div className="flex absolute -left-3 h-full items-center">
         <div
           className={`${
-            pathname === "/servers/1"
-              ? "rounded-2xl bg-brand text-white"
-              : "text-gray-100 hover:rounded-2xl hover:bg-brand hover:text-white"
-          } bg-gray-700  w-12 h-12 rounded-3xl  flex items-center justify-center  transition-all duration-200`}
-        >
-          S1
-        </div>
-      </Link>
-    </nav>
+            pathname === href
+              ? "h-10"
+              : "h-5 scale-0 opacity-0 group-hover:opacity-100 group-hover:scale-100"
+          } w-1 bg-white rounded-r transition-all duration-200 origin-left`}
+        ></div>
+      </div>
+      <div
+        className={`${
+          pathname === href
+            ? "rounded-2xl bg-brand text-white"
+            : "text-gray-100 group-hover:rounded-2xl group-hover:bg-brand group-hover:text-white bg-gray-700 rounded-3xl"
+        } flex items-center justify-center w-12 h-12  transition-all duration-200  group-active:translate-y-px overflow-hidden`}
+      >
+        {children}
+      </div>
+    </Link>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,28 @@
+import ChannelsSidebar from "@/app/components/channels-sidebar";
+
 export default function Home() {
-  return <main className="bg-gray-700 flex flex-col flex-1">Welcome</main>;
+  return (
+    <>
+      <div className="h-full w-60 bg-gray-800 overflow-y-scroll space-y-2">
+        <div className="flex items-center h-12 px-3 shadow-md">Dashboard</div>
+
+        <div className="flex-1 p-3 space-y-2 overflow-y-scroll text-gray-100">
+          Friends
+        </div>
+      </div>
+      <main className="bg-gray-700 flex flex-col flex-1">
+        <div className="flex items-center h-12 px-3 shadow-md">#general</div>
+        <div className="flex-1 p-3 space-y-4 overflow-y-scroll">
+          {[...Array(40)].map((_, i) => (
+            <p key={i}>
+              Message {i}. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Mollitia deleniti fugiat, unde explicabo nemo incidunt quam
+              similique assumenda dolorum vitae, molestiae quaerat cupiditate
+              esse optio error reiciendis, ab tempora dolore?
+            </p>
+          ))}
+        </div>
+      </main>
+    </>
+  );
 }
