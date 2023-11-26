5dc681db7bd2acb4771a08b5e45c35ad80c7ea7c
Boris <47457848+boris-chb@users.noreply.github.com>
2023-11-26 05:20:23 +0000

    styling channel sidebar

--- app/components/server-sidebar.tsx
+++ app/components/server-sidebar.tsx
@@ -1,24 +1,24 @@
-import { DiscordIcon } from "@/app/components/ui/icons";
-import NavLink from "@/app/components/nav-link";
-import Image from "next/image";
+import { Discord } from '@/app/components/ui/icons';
+import NavLink from '@/app/components/nav-link';
+import Image from 'next/image';
 
 const servers = [
-  { id: 1, img: "next.svg" },
-  { id: 2, img: "vercel.svg" },
+  { id: 1, img: 'next.svg' },
+  { id: 2, img: 'vercel.svg' },
 ];
 
 export default function ServersSidebar() {
   return (
-    <nav className="bg-gray-900 p-3 overflow-y-scroll flex flex-col gap-2">
-      <NavLink href="/">
-        <DiscordIcon className="h-5 w-7" />
+    <nav className='flex flex-col gap-2 overflow-y-scroll bg-gray-900 p-3'>
+      <NavLink href='/'>
+        <Discord className='h-5 w-7' />
       </NavLink>
 
-      <hr className="border-t-white/[.06] border-t-2 rounded mx-2" />
+      <hr className='mx-2 rounded border-t-2 border-t-white/[.06]' />
 
       {servers.map(({ id, img }) => (
         <NavLink key={id} href={`/servers/${id}`}>
-          <Image src={`/${img}`} fill alt="server icon" />
+          <Image src={`/${img}`} fill alt='server icon' />
         </NavLink>
       ))}
     </nav>
