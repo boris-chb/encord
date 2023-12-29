aafc6cf41916f55774dbb2c79876a041ef3a3679
boris-chb <boris.chobyrka@gmail.com>
2023-12-30 01:59:52 +0200

    removed serverId page (using route handler instead)

--- app/api/categories/route.ts
+++
@@ -1,7 +0,0 @@
-export async function GET(request: Request, { params }: any) {
-  const url = new URL(request.url);
-
-  console.log(params);
-
-  return Response.json({ route: '/api/categories' });
-}
--- app/servers/[serverId]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface ServerProps {
-  params: { serverId: string };
-}
-
-export default async function ServerPage({
-  params: { serverId },
-}: ServerProps) {
-  return <main>Select a channel</main>;
-}
